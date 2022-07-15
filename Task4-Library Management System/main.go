f69919a3236201b54d208714247d3c32f175c046
Sarthak Shastri <55555570+sarthak815@users.noreply.github.com>
2022-07-16 02:03:58 +0530

    Return From User pending

--- Task4-Library Management System/main.go
+++ Task4-Library Management System/main.go
@@ -56,26 +56,25 @@ type Book interface {
 	Name() string
 	Author() string
 	Borrow() bool
+	Return()
 }
 
-func createPhybook(btype BookType, name, author string) Books {
-	var phybook Books
-	phybook.B_type = btype
-	phybook.B_Name = name
-	phybook.B_Author = author
-	phybook.Capacity = 1
-	phybook.Borrowed = 0
-	return phybook
+func (book *Books) Init(btype BookType, name, author string, capacity int) {
+	book.B_type = btype
+	book.B_Name = name
+	book.B_Author = author
+	book.Capacity = capacity
+	book.Borrowed = 0
 }
-func createDigbook(btype BookType, name, author string, capacity int) Books {
-	var digBook Books
-	digBook.B_type = btype
-	digBook.B_Name = name
-	digBook.B_Author = author
-	digBook.Capacity = capacity
-	digBook.Borrowed = 0
-	return digBook
-}
+
+//func (digBook Books) Init(btype BookType, name, author string, capacity int) {
+//	digBook.B_type = btype
+//	digBook.B_Name = name
+//	digBook.B_Author = author
+//	digBook.Capacity = capacity
+//	digBook.Borrowed = 0
+//
+//}
 
 func (b Books) Booktype() string {
 	return [...]string{"eBook", "Audiobook", "Hardback", "Paperback", "Encyclopedia", "Magazine", "Comic"}[b.B_type]
@@ -94,6 +93,41 @@ func (b *Books) Borrow() bool {
 		b.Borrowed++
 	}
 	return borrowed
+}
+func (b *Books) Return() {
+	b.Borrowed--
+}
+func checkUserValidity(name string, lib Library) (bool, *Member) {
+	b := false
+	var member *Member
+	for i := range lib.Members {
+		if lib.Members[i].Name == name {
+			member = &lib.Members[i]
+			b = true
+			break
+		}
+	}
+	return b, member
+}
+func checkBookValidity(bname string, lib Library) (bool, *Books) {
+	bfound := false
+	var bookFound *Books
+	for i := range lib.BooksBorrowed {
+		if lib.BooksBorrowed[i].B_Name == bname {
+			bookFound = &lib.BooksBorrowed[i]
+			bfound = true
+			break
+		}
+	}
+	return bfound, bookFound
+}
+func printBookDetails(bookFound *Books) {
+	fmt.Println("Details of the book: ")
+	fmt.Println("Book Type: " + bookFound.Booktype())
+	fmt.Println("Book Name: " + bookFound.Name())
+	fmt.Println("Book Author: " + bookFound.Author())
+	fmt.Println("Book Issued")
+
 }
 
 type Member struct {
@@ -118,153 +152,171 @@ func main() {
 	var lib Library
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
-		fmt.Println("Enter 1.Enter Book in LibraryDB\n2.Enter Member Details\n3.Borrow a book\n4.Exit")
+		fmt.Println("Enter 1.Enter Librarian Interface\n2.Enter User Interface\n3.Exit")
 		var n int
 		fmt.Scanln(&n)
 		if n == 1 {
-			var bookS Books
-			//var typeBook string
-			var btype BookType
-			fmt.Println("Enter Book Type \n1.Ebook\n2. AudioBook\n3. HardBack\n4. PaperBack\n5. Encyclopedia\n6. Magazine\n7. Comic: ")
-			fmt.Scanln(&btype)
-			btype--
-			fmt.Println("Enter Name of book: ")
-			scanner.Scan()
-			name := scanner.Text()
-			fmt.Println("Enter Author of book: ")
-			scanner.Scan()
-			author := scanner.Text()
+			fmt.Println("Enter 1.Enter Book in LibraryDB\n2.Exit")
+			var n int
+			fmt.Scanln(&n)
+			if n == 1 {
 
-			if btype <= 1 {
-				var capac int
-				fmt.Println("Enter borrowing limit: ")
-				fmt.Scanln(&capac)
-				bookS = createDigbook(btype, name, author, capac)
-				fmt.Println(bookS)
-				lib.BooksBorrowed = append(lib.BooksBorrowed, bookS)
+				bookS := new(Books)
+				//var typeBook string
+				var btype BookType
+				fmt.Println("Enter Book Type \n1.Ebook\n2. AudioBook\n3. HardBack\n4. PaperBack\n5. Encyclopedia\n6. Magazine\n7. Comic: ")
+				fmt.Scanln(&btype)
+				btype--
+				fmt.Println("Enter Name of book: ")
+				scanner.Scan()
+				name := scanner.Text()
+				fmt.Println("Enter Author of book: ")
+				scanner.Scan()
+				author := scanner.Text()
 
-			}
-			if btype > 1 && btype <= 3 {
-				bookS = createPhybook(btype, name, author)
-				fmt.Println(bookS)
+				if btype <= 1 {
+					var capac int
+					fmt.Println("Enter borrowing limit: ")
+					fmt.Scanln(&capac)
+					bookS.Init(btype, name, author, capac)
+					fmt.Println(bookS)
+					lib.BooksBorrowed = append(lib.BooksBorrowed, *bookS)
 
+				}
+				if btype > 1 && btype <= 3 {
+					bookS.Init(btype, name, author, 1)
+					fmt.Println(bookS)
+
+				}
+				if btype > 3 {
+					var capac int
+					fmt.Println("Enter borrowing limit for digital: ")
+					fmt.Scanln(&capac)
+					bookS.Init(btype, name+"Digital", author, capac)
+					fmt.Println(bookS)
+					lib.BooksBorrowed = append(lib.BooksBorrowed, *bookS)
+					bookS.Init(btype, name+"Physical", author, 1)
+					lib.BooksBorrowed = append(lib.BooksBorrowed, *bookS)
+				}
+				fmt.Println(lib)
 			}
-			if btype > 3 {
-				var capac int
-				fmt.Println("Enter borrowing limit for digital: ")
-				fmt.Scanln(&capac)
-				bookS = createDigbook(btype, name+"Digital", author, capac)
-				lib.BooksBorrowed = append(lib.BooksBorrowed, bookS)
-				bookS = createPhybook(btype, name+"Physical", author)
-				lib.BooksBorrowed = append(lib.BooksBorrowed, bookS)
+			if n == 2 {
+				break
 			}
-			fmt.Println(lib)
+
 		}
 		if n == 2 {
-			var member Member
-			fmt.Println("Enter name:")
-			scanner.Scan()
-			name := scanner.Text()
-			var age int
-			fmt.Scanln(&age)
-			member = createMember(name, age)
-			lib.Members = append(lib.Members, member)
-			fmt.Println(lib)
+			fmt.Println("1.Enter Member Details\n2.Borrow a book\n3.Return Book\n4.Exit")
+			var n int
+			fmt.Scanln(&n)
+			if n == 1 {
+				var member Member
+				fmt.Println("Enter name:")
+				scanner.Scan()
+				name := scanner.Text()
+				var age int
+				fmt.Println("Enter age:")
+				fmt.Scanln(&age)
+				member = createMember(name, age)
+				lib.Members = append(lib.Members, member)
+				fmt.Println(lib)
 
-		}
-		if n == 3 {
-			var vermember *Member
-			fmt.Println("Enter your name: ")
-			scanner.Scan()
-			name := scanner.Text()
-			b := false
-			for i := range lib.Members {
-				if lib.Members[i].Name == name {
-					vermember = &lib.Members[i]
-					b = true
-					break
-				}
 			}
-			if b {
-				fmt.Println("Identity verified")
-				var btype BookType
-				fmt.Println("Enter BookType: \n1.Ebook\n2. AudioBook\n3. HardBack\n4. PaperBack\n5. Encyclopedia\n6. Magazine\n7. Comic: ")
-				fmt.Scanln(&btype)
-				if btype > 4 {
+			if n == 2 {
+				var vermember *Member
+				var b bool
+				fmt.Println("Enter your name: ")
+				scanner.Scan()
+				name := scanner.Text()
+				b, vermember = checkUserValidity(name, lib)
+				if b {
+					fmt.Println("Identity verified")
+					var btype BookType
+					fmt.Println("Enter BookType: \n1.Ebook\n2. AudioBook\n3. HardBack\n4. PaperBack\n5. Encyclopedia\n6. Magazine\n7. Comic: ")
+					fmt.Scanln(&btype)
 					fmt.Println("Enter Book name: ")
 					scanner.Scan()
 					bname := scanner.Text()
 					var digPhy int
-					fmt.Println("Enter Copy Type:\n1.Digital\n2.Physical")
-					fmt.Scanln(&digPhy)
-					if digPhy == 1 {
-						bname += "Digital"
-					}
-					if digPhy > 1 || digPhy < 1 {
-						bname += "Physical"
+					if btype > 4 {
+
+						fmt.Println("Enter Copy Type:\n1.Digital\n2.Physical")
+						fmt.Scanln(&digPhy)
+						if digPhy == 1 {
+							bname += "Digital"
+						}
+						if digPhy > 1 || digPhy < 1 {
+							bname += "Physical"
+						}
 					}
 					bfound := false
 					var bookFound *Books
-					for i := range lib.BooksBorrowed {
-						if lib.BooksBorrowed[i].B_Name == bname {
-							bookFound = &lib.BooksBorrowed[i]
-							bfound = true
-							break
-						}
-					}
+					bfound, bookFound = checkBookValidity(bname, lib)
 					if bfound {
 						if !bookFound.Borrow() {
 							fmt.Println("Book Unavailable")
 							continue
 						}
 						fmt.Println(*bookFound)
-						fmt.Println("Details of the book: ")
-						fmt.Println("Book Type: " + bookFound.Booktype())
-						fmt.Println("Book Name: " + bookFound.Name())
-						fmt.Println("Book Author: " + bookFound.Author())
 						vermember.BooksBorrowed = append(vermember.BooksBorrowed, *bookFound)
-						fmt.Println("Book Issued")
+						printBookDetails(bookFound)
 						fmt.Println(lib)
 					}
 					if !bfound {
 						fmt.Println("Book Not found!!")
 					}
+
 				} else {
+					fmt.Println("Details not found!! Please Register yourself!")
+				}
+			}
+			if n == 3 {
+				var b bool
+				fmt.Println("Enter your name: ")
+				scanner.Scan()
+				name := scanner.Text()
+				b, _ = checkUserValidity(name, lib)
+				if b {
+					fmt.Println("Identity verified")
+					var btype BookType
+					fmt.Println("Enter BookType: \n1.Ebook\n2. AudioBook\n3. HardBack\n4. PaperBack\n5. Encyclopedia\n6. Magazine\n7. Comic: ")
+					fmt.Scanln(&btype)
 					fmt.Println("Enter Book name: ")
 					scanner.Scan()
 					bname := scanner.Text()
-					bfound := false
-					var bookFound *Books
-					for i := range lib.BooksBorrowed {
-						if lib.BooksBorrowed[i].B_Name == bname {
-							bookFound = &lib.BooksBorrowed[i]
-							bfound = true
-							break
+					var digPhy int
+					if btype > 4 {
+
+						fmt.Println("Enter Copy Type:\n1.Digital\n2.Physical")
+						fmt.Scanln(&digPhy)
+						if digPhy == 1 {
+							bname += "Digital"
+						}
+						if digPhy > 1 || digPhy < 1 {
+							bname += "Physical"
 						}
 					}
+					bfound := false
+					var bookFound *Books
+					bfound, bookFound = checkBookValidity(bname, lib)
 					if bfound {
-						if !bookFound.Borrow() {
-							fmt.Println("Book Unavailable")
-							continue
-						}
-						fmt.Println(*bookFound)
-						fmt.Println("Details of the book: ")
-						fmt.Println("Book Type: " + bookFound.Booktype())
-						fmt.Println("Book Name: " + bookFound.Name())
-						fmt.Println("Book Author: " + bookFound.Author())
-						vermember.BooksBorrowed = append(vermember.BooksBorrowed, *bookFound)
-						fmt.Println("Book Issued")
+						bookFound.Return()
+						fmt.Println("Book Returned")
 						fmt.Println(lib)
 					}
 					if !bfound {
 						fmt.Println("Book Not found!!")
 					}
+
+				} else {
+					fmt.Println("Details not found!! Please Register yourself!")
 				}
-			} else {
-				fmt.Println("Details not found!! Please Register yourself!")
+			}
+			if n == 4 {
+				break
 			}
 		}
-		if n == 4 {
+		if n == 3 {
 			break
 		}
 
